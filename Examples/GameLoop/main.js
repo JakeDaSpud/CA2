ebcfcaa66893bb64cb4ad5e8f7289f8578a4b251
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-11-09 15:33:25 +0000

    Created GameLoop

--- Examples/GameLoop/main.js
+++ Examples/GameLoop/main.js
@@ -1,108 +1,69 @@
-//Can redefine var variables
-var num = 25;
-var str = "Hello World";
-console.log(num);
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-//Cannot redefine let variables
-let num2 = 25;
-let str2 = "hellow w0r1d"
-let isAvailable = true;
-let notDefined;
-let isNull = null;
-let obj = {firstName: "John", lastName: "Doe"};
+const playerWidth = 50;
+const playerHeight = 50;
+let playerX = 0;
+let playerY = canvas.height/2 - playerHeight/2;
 
-//If
-let condition = true;
-if (condition) {
-    console.log("condition is true");
-}
+const enemyWidth = 50;
+const enemyHeight = 50;
+let enemyX = canvas.width - enemyWidth;
+let enemyY = canvas.height/2 - enemyHeight/2;
 
-else {
-    console.log("condition is false");
-}
+//Keeps calling next frame in recursive loop
+function gameLoop() {
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = "blue";
+    ctx.fillRect(playerX, playerY, playerWidth, playerHeight);
 
+    ctx.fillStyle = "red";
+    ctx.fillRect(enemyX, enemyY, enemyWidth, enemyHeight);
 
-//Loops
-for (i = 0; i < 5; i++) {
-    console.log(i);
-}
+    const enemySpeed = 1;
+    const dx = playerX - enemyX;
+    const dy = playerY - enemyY;
+    const distance = Math.sqrt(dx*dx+dy*dy);
 
-let j = 0;
-
-while (j < 5) {
-    console.log(j);
-    j++;
-}
-
-//Making function
-function greet(name) {
-    console.log("Hello "+name);
-}
-
-greet("Jembo");
-
-//Arrays
-let fruits = ["Apple", "Orange", "Banana", "Mango"];
-console.log(fruits[1]);
-
-//Objects
-let person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 3
-}
-
-console.log(person.age);
-
-var varVariable = "I can be redeclared and am a var."
-let letVariable = "I can't be redeclared and am a let, I can be changed."
-const constVar = "I'm a const and can't be redeclared or changed."
-
-console.log(varVariable);
-console.log(letVariable);
-console.log(constVar);
-
-varVariable = "Updated var";
-letVariable = "Updated let";
-//constVar = "Updated const";
-
-class Car {
-    constructor (brand, model) {
-        this.brand = brand;
-        this.model = model;
+    if (distance > 0) {
+        enemyX += dx/distance * enemySpeed;
+        enemyY += dy/distance * enemySpeed;
     }
 
-    displayCar() {
-        return this.brand + " " + this.model;
+    const playerSpeed = 3;
+
+    if (keys.ArrowLeft && playerX > 0) {
+        playerX -= playerSpeed;
     }
+
+    if (keys.ArrowRight && playerX < canvas.width - playerWidth) {
+        playerX += playerSpeed;
+    }
+
+    if (keys.ArrowUp && playerY > 0) {
+        playerY -= playerSpeed;
+    }
+
+    if (keys.ArrowDown && playerY < canvas.height - playerHeight) {
+        playerY += playerSpeed;
+    }
+
+    requestAnimationFrame(gameLoop);
 }
 
-let myCar = new Car("Toyota", "Corolla");
-console.log(myCar.displayCar());
+//Keys and input
+let keys = [];
 
-//Inheritance
-class Model extends Car {
-    constructor (brand, model, year) {
-        super(brand, model);
-        this.year = year;
-    }
+//Event keydown makes event true
+document.addEventListener("keydown", event =>{
+    keys[event.key] = true;
+})
 
-    displayModel() {
-        return this.displayCar()+ " " + this.year;
-    }
-}
+//Event keyup makes event false
+document.addEventListener("keyup", event =>{
+    keys[event.key] = false;
+})
 
-let myCar2 = new Model("Toyoyoyo", "HUHUHUH", "2003");
-console.log(myCar2.displayModel());
-
-//Arrow Functions (lambdas??)
-const arr = [1, 2, 3, 4];
-const squares = arr.map((x) => x*x);
-console.log(squares);
-
-//Template Literals (printf lol)
-let nameGirl = "GIRLNAME";
-console.log(`Hello, ${nameGirl}!`);
-
-//import {hello} from "./myModule.js";
-//console.log(hello());
+requestAnimationFrame(gameLoop);
